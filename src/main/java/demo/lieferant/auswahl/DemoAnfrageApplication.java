4e151a6712df52b574e012fd1534c32459fcbcd4
Wang Gang <32674169+lukaswang@users.noreply.github.com>
2018-01-15 20:59:00 +0100

    Delete DemoAnfrageApplication.java

--- src/main/java/demo/lieferant/auswahl/DemoAnfrageApplication.java
+++
@@ -1,9 +0,0 @@
-package demo.lieferant.auswahl;
-
-import org.camunda.bpm.application.ProcessApplication;
-import org.camunda.bpm.application.impl.ServletProcessApplication;
-
-@ProcessApplication("app")
-public class DemoAnfrageApplication extends ServletProcessApplication {
-  // empty implementation
-}
