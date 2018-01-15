faf29f2b881a3c536c0e57b0b7dfd2a9f562fece
Wang Gang <32674169+lukaswang@users.noreply.github.com>
2018-01-15 19:53:01 +0100

    Add files via upload

---
+++ src/main/java/demo/lieferant/auswahl/DemoAnfrageApplication.java
@@ -0,0 +1,9 @@
+package demo.lieferant.auswahl;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("app")
+public class DemoAnfrageApplication extends ServletProcessApplication {
+  // empty implementation
+}
